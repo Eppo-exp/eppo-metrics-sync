0081916945c4ecba30cebde23d5ebff27cf65811
Eric Petzel <eric@geteppo.com>
2024-10-30 17:20:15 +0300

    allow fact sources to always fully refresh

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='eppo_metrics_sync',
-    version='0.0.8',
+    version='0.1.0',
     packages=find_packages(),
     install_requires=[
         'PyYAML', 'jsonschema', 'requests'
